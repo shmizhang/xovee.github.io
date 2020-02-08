19e918a4ee845782a23825dbfaa6f989684bed6a
Xovee Xu <xovee@appledeMacBook-Pro-4.local>
2020-02-08 16:38:36 +0800

    update

--- js/xovee.js
+++ js/xovee.js
@@ -6,12 +6,32 @@ $(document).ready(function(){
     // $('#project-title').mouseenter(function(){
     //     $('#project-form').show()
     // })
-    $('#project-title').mouseleave(function(){
-        $('#project-form').slideUp()
-        $('#project-title b').text('► Project')
+
+    // hide not selected entries
+    $('tbody').hide()
+    $('tbody.selected').show()
+
+
+    $('#project-title').on({
+        mouseleave: function(){
+            $('#project-form').hide(100)
+            $('#project-title b').text('► Project')
+            $('#project-title').css('background-color', '#f5f5f0')
+        },
+        mouseenter: function(){
+            $('#project-form').show(100)
+            $('#project-title b').text('▼ Project')
+            $('#project-title').css('background-color', '#ffffcc')
+        },
     })
-    $('#project-title').mouseenter(function(){
-        $('#project-form').slideDown()
-        $('#project-title b').text('▼ Project')
+
+    // project selector
+    $('#project-all').click(function(){
+        $('#project-table tbody').show()
     })
+    $('#project-selected').click(function(){
+        $('#project-table tbody').hide()
+        $('#project-table tbody.selected').show()
+    })
+    
 })
