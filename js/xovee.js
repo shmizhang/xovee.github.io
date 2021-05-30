4544ac8b4c198c7303bde3992723176db049e076
Xovee <xovee@live.com>
2021-05-30 20:19:34 +0800

    modified the looking of my home page

--- js/xovee.js
+++ js/xovee.js
@@ -3,32 +3,8 @@ $(document).ready(function(){
     curYear = (new Date).getFullYear();
     $('footer #current-year').text(curYear)
 
-    // show or hide links
-    $('#show-links').hide()
-    $('#show-links-button').click(function(){
-        $('#show-links').toggle(0, function(){
-            if ($.trim($('#show-links-button').text()) === 'More') {
-                $('#show-links-button').text('Less');
-            } else {
-                $('#show-links-button').text('More');        
-            }
-        })
-    })
-
     // hide news and show recent
     $('#news-lst li').hide().slice(0, 5).show()
-
-    // hide and show divs of articles, projects, ...
-    $('#undefined-content, #articles-content, #projects-content').hide()
-    $('#tabs span').each(function(){
-        $(this).click(function(){
-            $('#undefined-content, #articles-content, #projects-content').hide()
-            $('#' + $(this).attr('id') + '-content').show()
-            $('#tabs span').removeClass('tab-select tab-color')
-            $(this).addClass('tab-select tab-color')
-        })
-    })
-    $('#articles').click()
     
     // show only selected articles
     $('#article-selected').click(function(){
