ea9ec085a5ab6fd12faeddf5a1c6b976e8967ffc
Xovee <xovee@live.com>
2021-07-21 17:59:09 +0800

    update 'update date'

--- js/xovee.js
+++ js/xovee.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
-    // copyright year
+    // copyright year and update date
     curYear = (new Date).getFullYear();
     $('footer #current-year').text(curYear)
+    const curDate = Date.now()
+    const updateDate = new Date(curDate).toDateString().slice(4)
+    $('footer #update-date').text(updateDate)
 
     // hide news and show recent
     $('#news dd, dt').hide().slice(0, 10).show()
@@ -15,4 +18,5 @@ $(document).ready(function(){
         $('#articles-content p').show()
     })
     $('#article-selected').click()
+
 })
