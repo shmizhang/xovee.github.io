3a8a25c08111ddd01a9c2d1814a93199ed5674d7
xovee <xovee@live.com>
2021-07-26 16:28:55 +0800

    remove jquery

--- js/xovee.js
+++ js/xovee.js
@@ -1,33 +1,15 @@
 $(document).ready(function(){
     // copyright year and update date
     curYear = (new Date).getFullYear();
-    $('footer #current-year').text(curYear)
-    const curDate = Date.now()
-    const updateDate = new Date(curDate).toDateString().slice(4)
-    $('footer #update-date').text(updateDate)
+    document.getElementById('current-year').innerHTML = curYear;
+    const curDate = Date.now();
+    const updateDate = new Date(curDate).toDateString().slice(4);
+    document.getElementById('update-date').innerHTML = updateDate;
 
     // hide news and show recent
-    $('#news dd, dt').hide().slice(0, 10).show()
-    
-    // show only selected articles
-    $('#article-selected').click(function(){
-        $('#articles-content p').hide()
-        $('#articles-content p.selected').show()
-    })
-    $('#article-all').click(function(){
-        $('#articles-content p').show()
-    })
-    $('#article-selected').click()
-
-    // canv
-    $('#webweb-menu').hide()
-    $('#show-all-names').click(function(){
-        $('#showNodeNames-widget').click()
-    })
-    // $('#show-all-names').click()
-    $('#show-frequent-co-authors').click(function(){
-        $('#showFrequentCoAuthors-widget').click()
-    })
-
-    $('#nameToMatch-widget').prependTo('#show-certain-author')
+    const newestNews = document.querySelectorAll('#news dd, dt');
+    newestNews.forEach(element => element.style.display = 'none');
+    for (let i = 0; i < 10; i++) {
+        newestNews[i].style.display = '';
+    }
 })
