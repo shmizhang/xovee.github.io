f6cb5ed16aa7c721e7afb5fd3b674dec1fc6e2ac
Xovee <xovee@live.com>
2022-03-09 12:33:09 +0800

    update

--- plt/timeline/bokeh_try.py
+++ plt/timeline/bokeh_try.py
@@ -70,7 +70,8 @@ p = figure(title="Xovee's Timeline",
            sizing_mode='stretch_width', max_width=920, min_width=600,
            x_range=(to_date('2013/09/01'), to_date('2024/06/30')),
            y_range=(0, max_citations),
-           tools=['xpan', 'xwheel_zoom', 'reset',],  # ResetTool,
+           tools=['xpan', 'xwheel_zoom', 'reset', ],  # ResetTool,
+           toolbar_location=None,
            active_scroll='xwheel_zoom',
            # tooltips='hi',
            # output_backend="webgl",
@@ -81,10 +82,10 @@ p.outline_line_width = 0
 p.title.align = 'left'
 
 p.toolbar.logo = None
-p.toolbar_location = 'above'
-p.toolbar.autohide = True
+p.toolbar_location = None
+# p.toolbar.autohide = True
 
-# p.background_fill_color = None
+p.background_fill_color = None
 p.border_fill_color = None
 
 p.xaxis.major_tick_line_color = 'silver'
@@ -95,8 +96,8 @@ p.yaxis.major_tick_line_color = None
 p.yaxis.minor_tick_line_color = None
 p.yaxis.axis_line_color = None
 
-p.xgrid.grid_line_color = '#eee'
-p.ygrid.grid_line_color = '#eee'
+# p.xgrid.grid_line_color = '#ddd'
+# p.ygrid.grid_line_color = '#ddd'
 
 circles = p.circle(
     'date',
@@ -120,12 +121,6 @@ segments = p.segment(
 all_milestone_lines = list()
 
 
-# spans
-def draw_lines(date):
-    line = p.line(x=[date, date], y=[0, date], line_color='black', line_dash='dashed')
-    p.add_layout(line)
-
-
 def draw_milestone_annotation(data):
     label = Label(x=to_date(data[0]), y=128, x_units='data', y_units='screen', text=data[1], text_color='gray',
                   text_font_size='12px')
@@ -133,14 +128,15 @@ def draw_milestone_annotation(data):
     all_milestone_lines.append(label)
 
 
+# spans
 lines = p.segment(x0='date', y0=0,
                   x1='date', y1=max_citations,
-                  line_color='gray', line_dash='dashed', source=milestone_source)
+                  line_color='gray', line_dash='dashed', line_width=1.3, source=milestone_source)
 
 for milestone in milestones:
     draw_milestone_annotation(milestone)
 
-yaxis_label = Label(x=0, y=5, x_units='screen', y_units='screen', text='# citations', text_color='black',
+yaxis_label = Label(x=0, y=5, x_units='screen', y_units='screen', text=' # citations', text_color='black',
                     text_font_size='12px', text_alpha=.5)
 p.add_layout(yaxis_label)
 
